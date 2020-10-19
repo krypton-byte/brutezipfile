c613f94df42298bfb45b9bce4f0efc032c727476
Krypton-Byte <krypton-byte@localhost.localdomain>
2020-10-19 22:18:38 +0700

    Fix gui.py

--- gui.py
+++ gui.py
@@ -33,11 +33,11 @@ def progress():
 
 def zipFile():
     global fileZip
-    fileZip = filedialog.askopenfilename(initialdir="/home/krypton-byte/brutezip", title="File Manager", filetypes=(("Zip","*.zip"),("Files","*.*")))
+    fileZip = filedialog.askopenfilename(initialdir="./", title="File Manager", filetypes=(("Zip","*.zip"),("Files","*.*")))
     print(fileZip)
 def wordlist():
     global wordlis
-    wordlis = filedialog.askopenfilename(initialdir="/home/krypton-byte/brutezip", title="File Manager", filetypes=(("Plain Text","*.txt"),("Files","*.*")))
+    wordlis = filedialog.askopenfilename(initialdir="./", title="File Manager", filetypes=(("Plain Text","*.txt"),("Files","*.*")))
     print(wordlis)
 def main():
     win=tk.Tk()
